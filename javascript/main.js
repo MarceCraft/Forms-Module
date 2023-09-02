aa711a4a9b5aa1c99fe79f25808e492c2154d3d4
nombresito <rmarcelo.osorio@gmail.com>
2023-09-02 00:16:44 -0400

    se agregaron los draggables

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,16 @@
 'use strict';
 const zone = document.querySelector(".zone");
 const video = document.getElementById("video");
+
+zone.addEventListener('dragenter', e => {
+    console.log('enter',e);
+})
+
+zone.addEventListener('dragover',e => {
+    e.preventDefault();
+    console.log('over',e);
+})
+
+zone.addEventListener('drop',e => console.log('drop',e));
+
+zone.addEventListener('dragleave',e => console.log('leave',e));
